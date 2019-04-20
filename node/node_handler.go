039e783065767ea6d9dc38f8f5d8c7d2780dc116
Leo Chen <leo@harmony.one>
2019-04-20 06:30:02 +0000

    [log] reduce number of logs
    Signed-off-by: Leo Chen <leo@harmony.one>

--- node/node_handler.go
+++ node/node_handler.go
@@ -119,10 +119,8 @@ func (node *Node) messageHandler(content []byte, sender string) {
 	case proto.Consensus:
 		msgPayload, _ := proto.GetConsensusMessagePayload(content)
 		if consensusObj.IsLeader {
-			utils.GetLogInstance().Info("NET: Leader received consensus message")
 			consensusObj.ProcessMessageLeader(msgPayload)
 		} else {
-			utils.GetLogInstance().Info("NET: Validator received consensus message")
 			consensusObj.ProcessMessageValidator(msgPayload)
 			// TODO(minhdoan): add logic to check if the current blockchain is not sync with other consensus
 			// we should switch to other state rather than DoingConsensus.
@@ -131,10 +129,8 @@ func (node *Node) messageHandler(content []byte, sender string) {
 		msgPayload, _ := proto.GetDRandMessagePayload(content)
 		if node.DRand != nil {
 			if node.DRand.IsLeader {
-				utils.GetLogInstance().Info("NET: DRand Leader received message")
 				node.DRand.ProcessMessageLeader(msgPayload)
 			} else {
-				utils.GetLogInstance().Info("NET: DRand Validator received message")
 				node.DRand.ProcessMessageValidator(msgPayload)
 			}
 		}
@@ -301,7 +297,6 @@ func (node *Node) VerifyNewBlock(newBlock *types.Block) bool {
 // 1. add the new block to blockchain
 // 2. [leader] send new block to the client
 func (node *Node) PostConsensusProcessing(newBlock *types.Block) {
-	utils.GetLogInstance().Info("PostConsensusProcessing")
 	if node.Consensus.IsLeader {
 		node.BroadcastNewBlock(newBlock)
 	} else {
@@ -400,7 +395,6 @@ func (node *Node) pingMessageHandler(msgPayload []byte, sender string) int {
 
 // SendPongMessage is the a goroutine to periodcally send pong message to all peers
 func (node *Node) SendPongMessage() {
-	utils.GetLogInstance().Info("Starting Pong routing")
 	tick := time.NewTicker(2 * time.Second)
 	tick2 := time.NewTicker(120 * time.Second)
 
@@ -567,8 +561,6 @@ func (node *Node) epochShardStateMessageHandler(msgPayload []byte) int {
 }
 
 func (node *Node) processEpochShardState(epochShardState *types.EpochShardState) {
-	utils.GetLogInstance().Error("[Received shard state]", "shardState", epochShardState)
-
 	shardState := epochShardState.ShardState
 	epoch := epochShardState.Epoch
 
